bc3e6ca263e5fe0597a5204b707814c3a1526992
Rick Dalley <rick@codemojo.ca>
2020-01-08 19:40:54 -0500

    Add files via upload

---
+++ server.go
@@ -0,0 +1,35 @@
+//
+// Hello World Zeromq server
+//
+// Author: Aaron Raddon   github.com/araddon
+// Requires: http://github.com/alecthomas/gozmq
+//
+package main
+
+import (
+	"fmt"
+	"time"
+
+	zmq "github.com/pebbe/zmq4"
+)
+
+func main() {
+	context, _ := zmq.NewContext()
+	socket, _ := context.NewSocket(zmq.REP)
+	defer context.Term()
+	defer socket.Close()
+	socket.Bind("tcp://*:5555")
+
+	// Wait for messages
+	for {
+		msg, _ := socket.Recv(0)
+		println("Received ", string(msg))
+
+		// do some fake "work"
+		time.Sleep(time.Second)
+
+		// send reply back to client
+		reply := fmt.Sprintf("World")
+		socket.Send(reply, 0)
+	}
+}
